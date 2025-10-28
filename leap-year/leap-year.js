e754df8a272863d7b5c68ceb4aacd21e1cb95539
XavierExCreator <xavier.ex.pro@gmail.com>
2025-10-28 15:30:52 -0400

    Added logic of leap-year.js, imported moments npm into ac-backedn-node-4-third-party-modules.

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -5,3 +5,32 @@ Make an app that determines if a given year is a leap year!
 
 Read the instructions in the README.md file in this folder.
 ------------------------------ */
+
+//ES Module syntax
+// import fs from "fs/promises";
+
+import moment from "moment";
+
+
+async function isItALeapYear() {
+
+let year = Number(process.argv[2]);
+const question = moment([year]).isLeapYear(); // True or False
+console.log(question)
+console.log(year)
+if (question === true && year === Number(year)) {
+    console.log(`${year} is a leap year!`);
+} else if (question === false && year === Number(year)) {
+    console.log(`${year} is not a leap year!`);
+} else {
+    console.log(`Is this there a letter? Is this a valid year? Please input a valid year that's been logged and make sure there's no letters when inputting the year. Thank you!`);
+}
+}
+
+// const secondQuestion = moment([process.argv[3]]).isLeapYear(); // false
+// console.log("Is 2001 a leap year?",secondQuestion);
+
+// const thirdQuestion = moment([process.argv[4]]).isLeapYear(); // false
+// console.log("Is 2100 a leap year?",thirdQuestion)
+
+isItALeapYear();
