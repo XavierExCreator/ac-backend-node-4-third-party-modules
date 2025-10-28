86e605876764d084b3a77d7b154b7d17a9d7b391
XavierExCreator <xavier.ex.pro@gmail.com>
2025-10-28 16:57:12 -0400

    Added logic to the sign-finder by downloading npm horoscope. Made the horoscope calculator tell the user their zodiac sign and horoscope sign based on month, day and year.

--- sign-finder/sign-finder.js
+++ sign-finder/sign-finder.js
@@ -5,3 +5,20 @@ Create a Node.js app that determines the astrological and zodiac signs for the u
 
 Read the instructions in the README.md file in this folder.
 ---------------------------- */
+
+import horoscope from "horoscope";
+
+function findZodiacSign() {
+
+    let usersMonthAndDate = horoscope.getSign({month: Number(process.argv[2]), day: Number(process.argv[3]) })
+    
+    let usersYear = horoscope.getZodiac(Number(process.argv[4]))
+
+    if(usersMonthAndDate) {
+        console.log(`Your astrological sign is ${usersMonthAndDate} and your zodiac sign is ${usersYear}`)
+    } else {
+        console.log("System has not caught the error, somethine else is wrong");
+    }
+}
+
+findZodiacSign();
